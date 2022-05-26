7cf09e5d95ff631079b25dd0aab53cf24989a12e
Shaurya Shubham <me@shauryashubham.com>
2022-05-27 00:15:20 +0530

    Add ruby code that actually generates a "website"

--- Rakefile
+++ Rakefile
@@ -1,5 +1,5 @@
 task default: %w[ build ]
 
 task :build do
-    sh "cat /dev/urandom -v | head -n 7"
+    ruby "src/main.rb"
 end
--- src/main.rb
+++ src/main.rb
@@ -1,11 +1,38 @@
 require 'bundler/inline'
 
 gemfile do
-  source 'https://rubygems.org'
-  gem 'asciidoctor'
-  gem 'asciidoctor-html5s'
+    source 'https://rubygems.org'
+    gem 'asciidoctor'
+    gem 'asciidoctor-html5s'
+    gem 'mustache'
 end
 
-puts "Hello world, you're an asshole."
-puts "asciidoctor version is #{Asciidoctor::VERSION}"
-puts "HTML5s extension version is #{Asciidoctor::Html5s::VERSION}"
+OUT_DIRECTORY = "./out"
+
+def pipeline(file_list, asciidoctor_config, mustache_config)
+    mustache_instance = Mustache.new
+    mustache_instance.template_file = mustache_config[:template_file]
+    mustache_instance.template_path = mustache_config[:template_path]
+    mustache_instance.template_extension = mustache_config[:template_extension]
+
+    file_list.map do |file_name|
+        contents = File.read file_name
+        document = Asciidoctor.load contents, asciidoctor_config
+        mustache_attrs = document.attributes.merge({content: document.convert})
+        puts mustache_instance.render mustache_attrs
+        new_name = "#{OUT_DIRECTORY}/#{File.dirname file_name}/#{File.basename file_name, '.*'}.html"
+    end
+end
+
+asciidoctor_config = {
+    converter: Asciidoctor::Html5s::Converter,
+    standalone: false,
+}
+
+mustache_config = {
+    template_path: "./templates",
+    template_extension: "mst",
+    template_file: "./templates/article.mst",
+}
+
+pipeline Dir.glob("articles/**.adoc"), asciidoctor_config, mustache_config
